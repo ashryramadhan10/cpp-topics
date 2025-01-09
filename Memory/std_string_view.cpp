6580eccb6fcdf22e2ca6b7c25ceea7de5534f311
Ashry Ramadhan <ashry.ramadhan@webscientia.id>
2025-01-09 19:52:40 +0700

    add: std_string_view.cpp

---
+++ Memory/std_string_view.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string_view>
+
+void printSubstring(std::string_view sv) {
+    std::cout << sv << std::endl;
+}
+
+
+int main(int argc, char* argv[]) {
+    
+    std::string str = "Hello, world!";
+    std::string_view sv = str.substr(7, 5);
+
+    printSubstring(sv);
+    return 0;
+}
